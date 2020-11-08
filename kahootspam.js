f6c02402f52eceea53d97f6e933d0db3f8ee4e3a
MrNewdary <72687020+MrNewdary@users.noreply.github.com>
2020-11-08 17:44:15 +1300

    Update kahootspam.js

--- kahootspam.js
+++ kahootspam.js
@@ -1,39 +1,3 @@
-/*
-const Kahoot = require("kahoot.js-updated")
-
-
-
-class KahootMan {
-    constructor(name, PIN){
-    this.bot = new Kahoot()
-    this.name = name
-    this.bot.join(PIN, this.name)
-    this.on_join = function(){
-        console.log("Joined " + this.name)
-    }
-    this.on_question = function(question){
-        console.log("Questioned")
-        question.answer(0)
-    }
-    this.bot.on("Joined", this.on_join)
-    this.bot.on("QuestionStart", this.on_question)
-
-    }
-    
-}
-
-var tailstring = ""
-var users = []
-function spam(PIN, Name, Amount){
-    for (i = 0; i < Amount; i++)
-    {
-        var name = Name + tailstring
-        users.push(new KahootMan(name + tailstring, PIN))
-        tailstring += " "
-    }
-}
-*/
-
 var button = document.getElementById("sendButton")
 button.onClick = function(){
     document.getElementById("PIN").innerHTML = "69"
